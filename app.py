e68e856f3e94c6e479d7ab001e5bb68e284e146f
Muskan <mkmuskanbegam@gmail.com>
2025-06-23 14:31:23 +0530

    news

--- app.py
+++ app.py
@@ -336,8 +336,8 @@ scheduler.add_job(
     id='news_scraping_job',
     func=scheduled_scrape,
     trigger='cron',
-    hour=12,  # 10 AM Kolkata time
-    minute=32,
+    hour=14,  # 10 AM Kolkata time
+    minute=40,
     timezone=kolkata_tz  # Explicitly set timezone
 )
 
